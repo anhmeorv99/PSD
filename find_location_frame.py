46ee1f8805f2e9e408fcfa456c1462d2f3c41f4b
anhmeorv99 <anhmeorv99@gmail.com>
2021-03-24 17:58:25 +0700

    find location frame type=shape, font size text

---
+++ find_location_frame.py
@@ -0,0 +1,62 @@
+import re
+
+from psd_tools import PSDImage
+import json
+
+psd = PSDImage.open('/home/anhmeo/Desktop/1000x1000 (1).psd')
+
+
+def find_location_and_text(root):
+    list_frame = []
+    list_text = []
+    obj = []
+    for layer in reversed(list(root.descendants())):
+        if layer.kind == 'shape':
+            list_frame.append(
+                {
+                    "name": layer.name,
+                    "position": {
+                        "X": layer.offset[0],
+                        "Y": layer.offset[1]
+                    },
+                    "size": {
+                        "width": layer.size[0],
+                        "height": layer.size[1]
+                    }
+
+                }
+            )
+        if layer.kind == 'type':
+            data_ft = str(layer.engine_dict)
+            len_text = 0
+            text = re.findall(r"{'Text': '(.*)'}", data_ft)
+            font_size = re.findall(r"'FontSize': (\d+.\d+),", data_ft)
+            if len(text) > 0:
+                len_text = len(text[0].replace('\\r', ''))
+
+            if len(font_size) > 0:
+                font_size = font_size[0]
+                list_text.append(
+                    {
+                        "name": layer.name,
+                        "data": {
+                            "length_text": len_text,
+                            "font_size": float(font_size)
+                        }
+                    }
+                )
+
+    obj = [
+        {
+            "type": "frame",
+            "data": list_frame
+        },
+        {
+            "type": "text",
+            "data": list_text
+        }
+    ]
+    return json.dumps(obj)
+
+
+print(find_location_and_text(psd))
