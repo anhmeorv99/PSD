08d0c672325d3f6ce4254653c0cb5436ed18459c
anhmeorv99 <anhmeorv99@gmail.com>
2021-03-25 10:08:33 +0700

    fix bug params

--- render_text_img.py
+++ render_text_img.py
@@ -1,14 +1,22 @@
 from PIL import Image, ImageDraw, ImageFont
+import re
+from psd_tools import PSDImage
 
 
-def write_text_file_png(save_to, size, font_size, tff, text, location, color):
+def write_text_file_png(save_to, size, font_size, ttf, text, location, color):
+    if not save_to or '.png' not in save_to:
+        print('cant find format of image')
+        return
+    fnt = None
     img = Image.new('RGBA', size, (255, 0, 0, 0))
-
-    fnt = ImageFont.truetype(tff, font_size)
+    if ttf:
+        fnt = ImageFont.truetype(ttf, font_size)
     d = ImageDraw.Draw(img)
     d.text(location, text, font=fnt, fill=color)
 
     img.save(f'{save_to}')
+    print(f'save success {save_to}')
 
 
-write_text_file_png('test_text.png', (1000, 1000), 15, './font/OpenSans-Bold.ttf', "Xin Chao", (723, 140), (255, 255, 0))
+write_text_file_png('test_text.png', (1000, 1000), 15, './font/OpenSans-Bold.ttf', "Xin Chao", (723, 140),
+                    (255, 255, 0))
