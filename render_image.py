97862fe17e8e1df77306007dc2b85fdc71195089
anhmeorv99 <anhmeorv99@gmail.com>
2021-03-26 00:00:49 +0700

    add render thumbnail image

--- render_image.py
+++ render_image.py
@@ -1,11 +1,12 @@
 import concurrent
 import json
 import os
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor
 import find_location_frame
 from psd_tools import PSDImage
 
 list_image_object = []
+list_image_object_thumbnail = []
 
 
 def send_with_thread_executor(max_workers):
@@ -19,6 +20,17 @@ def send_with_thread_executor(max_workers):
             )
 
 
+def send_with_thread_executor_thumbnail(max_workers):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = []
+        for image in list_image_object:
+            futures.append(
+                executor.submit(
+                    write_file_thumbnail_png, image[0], image[1]
+                )
+            )
+
+
 def gen_path(current_path, branch):
     if len(branch) == 1:
         return current_path
@@ -26,7 +38,7 @@ def gen_path(current_path, branch):
 
         branch.reverse()
         for index in range(0, len(branch) - 1, 1):
-            if os.path.exists(f"{current_path}/{branch[index]}"):
+            if os.path.exists(f'{current_path}/{branch[index]}'):
                 current_path += f'/{branch[index]}'
                 continue
             os.mkdir(os.path.join(current_path, branch[index]))
@@ -86,6 +98,15 @@ def write_file_png(name, path, branch, current_path):
         print(f'save successfully file: {name}')
 
 
+def write_file_thumbnail_png(layer, current_path):
+    if not os.path.isfile(f'{current_path}/{layer.name}.png'):
+        layer.visible = True
+        image = layer.composite()
+        image.save(f'{current_path}/{layer.name}.png')
+        print(f'save successfully file: {layer.name}')
+        layer.visible = False
+
+
 def render_img(layers, path):
     list_layers = []
     for layer in layers:
@@ -98,13 +119,13 @@ def render_img(layers, path):
                     image.save(f'./lab1/{layer.name}.png')
                     print(f'save successfully file: {layer.name}')
                 list_layers.append({
-                    "name": layer.name,
-                    "url": f"./lab1/{layer.name}.png"
+                    'name': layer.name,
+                    'url': f'./lab1/{layer.name}.png'
                 })
                 continue
             if 'text -' in layer.name.lower():
                 list_layers.append({
-                    f"{layer.name}": find_location_frame.find_location_and_text(layer)
+                    f'{layer.name}': find_location_frame.find_location_and_text(layer)
                 })
                 # with open(f'./output_json/property_frame_and_text_{layer.name}.json', 'w') as f:
                 #     f.write(find_location_frame.find_location_and_text(layer))
@@ -112,7 +133,7 @@ def render_img(layers, path):
                 continue
             layer.visible = True
             obj = {
-                f"{layer.name}": render_img(layer, path),
+                f'{layer.name}': render_img(layer, path),
             }
             list_layers.append(obj)
         else:
@@ -130,8 +151,8 @@ def render_img(layers, path):
                 continue
 
             list_layers.append({
-                "name": f"{layer.name}",
-                "url": f"{current_path}/{layer.name}.png"
+                'name': f'{layer.name}',
+                'url': f'{current_path}/{layer.name}.png'
             })
 
             list_image_object.append([layer.name, path, branch, current_path])
@@ -142,19 +163,86 @@ def render_img(layers, path):
     return list_layers
 
 
-def start():
-    psd = PSDImage.open('/home/anhmeo/Desktop/1000x1000 (1).psd')
+def render_img_thumbnail(layers, path):
+    list_layers = []
+    for layer in layers:
+        if layer.kind == 'group':
+            if layer.name.lower() == 'background':
+                if not os.path.isfile(f'./lab1/thumbnail/{layer.name}.png'):
+                    layer.visible = True
+                    image = layer.composite()
+                    image.save(f'./lab1/thumbnail/{layer.name}.png')
+                    layer.visible = False
+                    print(f'save successfully file: {layer.name}')
+                list_layers.append({
+                    'name': layer.name,
+                    'url': f'./lab1/thumbnail/{layer.name}.png'
+                })
+                continue
+            if 'text -' in layer.name.lower():
+                list_layers.append({
+                    f'{layer.name}': find_location_frame.find_location_and_text(layer)
+                })
+                # with open(f'./output_json/property_frame_and_text_{layer.name}.json', 'w') as f:
+                #     f.write(find_location_frame.find_location_and_text(layer))
+                #     f.close()
+                continue
+            layer.visible = True
+            obj = {
+                f'{layer.name}': render_img_thumbnail(layer, path),
+            }
+            list_layers.append(obj)
+        else:
+
+            tmp = layer
+            branch = []
+            current_path = './lab1/thumbnail'
+            while tmp.parent is not None:
+                branch.append(tmp.name)
+                tmp = tmp.parent
+            current_path = gen_path(current_path, branch)
+
+            if layer.name.lower() == 'background' and layer.parent.parent:
+                write_file_thumbnail_png(layer, current_path)
+                continue
+
+            list_layers.append({
+                'name': f'{layer.name}',
+                'url': f'{current_path}/{layer.name}.png'
+            })
+
+            list_image_object_thumbnail.append([layer, current_path])
+            if len(list_image_object) > 10:
+                send_with_thread_executor_thumbnail(5)
+                list_image_object_thumbnail.clear()
+
+    return list_layers
+
+
+def start(path):
+    psd = PSDImage.open(path)
     set_visible_all(psd, False)
     set_visible(psd, False)
 
     content = {
-        "Root": render_img(psd, '/home/anhmeo/Desktop/1000x1000 (1).psd')
+        "Root": render_img(psd, path),
+    }
+
+    psd = PSDImage.open(path)
+
+    content_thumbnail = {
+        "thumbnail": render_img_thumbnail(psd, path)
+    }
+
+    content_output = {
+        'Root': content['Root'],
+        'thumbnail': content_thumbnail['thumbnail']
     }
 
     with open('./output.json', 'w') as f:
-        f.write(json.dumps(content))
+        f.write(json.dumps(content_output))
         f.close()
 
 
 if __name__ == '__main__':
-    start()
+    start('/home/anhmeo/Desktop/Once upon a time there was a girl who loves dogs. The end..psd')
