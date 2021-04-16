133fa8a4116713a0f99ae196e2abc57f327cc20c
Zic <zickieloox@gmail.com>
2021-04-16 15:44:01 +0700

    feat: get text_color and text_content get "ARGB" in psd text data and convert it to hex BREAKING CHANGE: correct spelling of some variables

--- find_location_frame.py
+++ find_location_frame.py
@@ -5,25 +5,30 @@ from psd_tools import PSDImage
 
 
 def find_location_and_text(root):
-    list_frame = []
-    list_text = []
-    obj = []
+    text_list = []
     for layer in reversed(list(root.descendants())):
         if layer.kind == 'type':
 
-            data_ft = str(layer.engine_dict)
-            font_name = re.findall(r"'FontSet': \[{'Name': '(.*)'", str(layer.resource_dict))[0]
+            text_data = str(layer.engine_dict)
+            # print(text_data)
+            font_name = re.findall(
+                r"'FontSet': \[{'Name': '(.*)'", str(layer.resource_dict))[0]
             font_name = font_name.split(',')[0].replace('\'', '')
-            len_text = 0
-            text = re.findall(r"{'Text': '(.*)'}", data_ft)
-            font_size = re.findall(r"'FontSize': (\d+.\d+),", data_ft)
-            if len(text) > 0:
-                len_text = len(text[0].replace('\\r', ''))
+
+            text_content = re.findall(
+                r"{'Text': '(.*)'}", text_data)[0].replace('\\r', '')
+            text_length = len(text_content)
+
+            text_color = re.findall(
+                r"'FillColor': {'Type': 1, 'Values': \[(.+?)]}", text_data)[0].split(', ')
+            text_color = rgb_hex(text_color)
+
+            font_size = re.findall(r"'FontSize': (\d+.\d+),", text_data)
             if len(font_size) == 0:
-                font_size = re.findall(r"'FontSize': (\d+),", data_ft)
+                font_size = re.findall(r"'FontSize': (\d+),", text_data)
             if len(font_size) > 0:
                 font_size = font_size[0]
-                list_frame.append(
+                text_list.append(
                     {
                         'name': layer.name,
                         'position': {
@@ -35,18 +40,19 @@ def find_location_and_text(root):
                             'height': layer.size[1]
                         },
                         'data': {
-                            'length_text': len_text,
+                            'text_content': text_content,
+                            'text_length': text_length,
                             'font_size': float(font_size),
-                            'font_name': font_name
+                            'font_name': font_name,
+                            'text_color': text_color
                         }
 
                     }
                 )
 
-    return list_frame
+    return text_list
 
 
-#
-# psd = PSDImage.open('/home/anhmeo/Desktop/dog1 (1).psd')
-#
-# print(json.dumps(find_location_and_text(psd)))
+def rgb_hex(color):  # https://stackoverflow.com/a/48288173/7182363
+    color = [255*float(x) for x in color[1:4]]
+    return '#' + ''.join(['{:02X}'.format(int(round(x))) for x in color])
